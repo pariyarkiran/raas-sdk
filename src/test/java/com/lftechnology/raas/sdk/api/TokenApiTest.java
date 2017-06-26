21718ebce08b890c63c476f744078923a3644d09
lf-kiranpariyar <kiranpariyar@lftechnology.com>
2017-06-26 15:27:13 +0545

    dev: Add token api

---
+++ src/test/java/com/lftechnology/raas/sdk/api/TokenApiTest.java
@@ -0,0 +1,60 @@
+package com.lftechnology.raas.sdk.api;
+
+import com.lftechnology.raas.sdk.constants.CommonConstant;
+import com.lftechnology.raas.sdk.dto.Token;
+import com.lftechnology.raas.sdk.exception.ApiException;
+import com.lftechnology.raas.sdk.pojo.ListResponse;
+import com.lftechnology.raas.sdk.util.ApiUtil;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author Kiran Pariyar <kiranpariyar@lftechnology.com>
+ */
+public class TokenApiTest {
+
+    private String baseUrl = "https://qa.raas.lftechnology.com/v1/";
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void shouldThrowApiExceptionWhenForInvalidCredentials() throws IOException{
+        //Given
+        TokenApi tokenApi = new TokenApi(this.baseUrl, getInvalidHeader());
+
+        //When-Then
+        thrown.expect(ApiException.class);
+        tokenApi.getToken(getSenderId());
+    }
+
+    @Test
+    public void testGetToken() throws IOException{
+        //Given
+        TokenApi tokenApi = new TokenApi(this.baseUrl, ApiUtil.getDefaultHeader());
+
+        //When
+        Token token = tokenApi.getToken(getSenderId());
+
+        //Then
+        Assert.assertNotNull(token);
+    }
+
+    private UUID getSenderId(){
+        return UUID.fromString("ea27c788-579b-48f8-a17e-756c9a93699f");
+    }
+
+    private Map<String,String> getInvalidHeader(){
+        Map<String,String> headers = new HashMap<>();
+        headers.put(CommonConstant.HTTP_CLIENT_ID, "R0SbAYpVieJ7BNQSe");
+        headers.put(CommonConstant.HTTP_CLIENT_SECRET, "04DYQmXqzmBbyJvN");
+        return headers;
+    }
+}
